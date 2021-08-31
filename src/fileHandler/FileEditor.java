034053be89cf50aa5593e3fcc87490b84c623e9f
MarcosVieira <marcosvieira1988@gmail.com>
2021-08-31 00:48:01 -0300

    Versão 5.0.2 - Iniciando gitignore

--- src/fileHandler/FileEditor.java
+++ src/fileHandler/FileEditor.java
@@ -20,38 +20,44 @@ public class FileEditor {
         try {
             FileInputStream inputStream = new FileInputStream(path);
             Workbook workbook = WorkbookFactory.create(inputStream);
-
-            Sheet sheet = workbook.getSheetAt(0);
-            Cell monthReference = sheet.getRow(3).getCell(0);
-            monthReference.setCellValue(new SimpleDateFormat("dd/MM/yyyy").parse(date));
-
-            for (int row = 0; row < scheduleValues.getRowCount(); row++) {
-                for (int col = 0; col < scheduleValues.getColumnCount(); col++) {
-                    sheet.getRow(row + 6).getCell(col).setCellValue(scheduleValues.getValueAt(row, col).toString());
-                }
-            }
-
-            Cell normalEH = sheet.getRow(43).getCell(1);
-            normalEH.setCellValue(footer.getNormalEH());
-            Cell specialEH = sheet.getRow(44).getCell(1);
-            specialEH.setCellValue(footer.getSpecialEH());
-            Cell warningHours = sheet.getRow(45).getCell(1);
-            warningHours.setCellValue(footer.getWarningHours());
-            Cell nightlyHours = sheet.getRow(46).getCell(1);
-            nightlyHours.setCellValue(footer.getNightlyHours());
-
+            inputDataIntoFile(workbook, date, scheduleValues, footer);
             inputStream.close();
-
             FileOutputStream outputStream = new FileOutputStream(path);
             workbook.write(outputStream);
             workbook.close();
             outputStream.close();
             JOptionPane.showMessageDialog(null, "Arquivo Gravado com Sucesso", "Sucesso",
                     JOptionPane.INFORMATION_MESSAGE);
-        } catch (IOException | EncryptedDocumentException | ParseException ex) {
+        } catch (IOException | EncryptedDocumentException ex) {
             JOptionPane.showMessageDialog(null, "Arquivo não encontrado ou já em uso, " +
                     "certifique - se de que esteja fechado ou caminho correto", "Erro", JOptionPane.ERROR_MESSAGE);
             ex.printStackTrace();
         }
+    }
+
+    public void inputDataIntoFile(Workbook workbook, String date, DefaultTableModel scheduleValues, FooterDTO footer) {
+        Sheet sheet = workbook.getSheetAt(0);
+        Cell monthReference = sheet.getRow(3).getCell(0);
+        try {
+            monthReference.setCellValue(new SimpleDateFormat("dd/MM/yyyy").parse(date));
+        } catch (ParseException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao processar a data de referência",
+                    "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+
+        for (int row = 0; row < scheduleValues.getRowCount(); row++) {
+            for (int col = 0; col < scheduleValues.getColumnCount(); col++) {
+                sheet.getRow(row + 6).getCell(col).setCellValue(scheduleValues.getValueAt(row, col).toString());
+            }
+        }
+
+        Cell normalEH = sheet.getRow(43).getCell(1);
+        normalEH.setCellValue(footer.getNormalEH());
+        Cell specialEH = sheet.getRow(44).getCell(1);
+        specialEH.setCellValue(footer.getSpecialEH());
+        Cell warningHours = sheet.getRow(45).getCell(1);
+        warningHours.setCellValue(footer.getWarningHours());
+        Cell nightlyHours = sheet.getRow(46).getCell(1);
+        nightlyHours.setCellValue(footer.getNightlyHours());
     }
 }
